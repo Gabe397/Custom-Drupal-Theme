1eeb28721999bda4688c7eeb490b0c623deef046
gabe3 <Dinya7991>
2019-04-10 20:03:19 -0400

    Task: Modified Template Files

--- drupal-7.64/sites/all/themes/myTheme/templates/node.tpl.php
+++ drupal-7.64/sites/all/themes/myTheme/templates/node.tpl.php
@@ -85,9 +85,7 @@
   <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
+    <?php print render($content['body']); ?>
   <?php print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
